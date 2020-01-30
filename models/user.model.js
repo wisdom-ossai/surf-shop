cbfdd44cd41664145d7e5b3726b1158b8c0f7ca0
Chuks Ossai <ossaiwisdom48@gmail.com>
2020-01-30 01:00:35 +0100

    Complete barebone for user model

---
+++ models/user.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema({
+    email: { type: String },
+    password: { type: String },
+    username: { type: String },
+    image: { type: String },
+    posts: {
+        type: Schema.Types.ObjectId,
+        ref: 'Post'
+    }
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
